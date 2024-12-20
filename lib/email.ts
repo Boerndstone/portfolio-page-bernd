acf22d5b0158d67c4c1011825e106376fa62eedb
Bernd Ullmann <info@berndullmann.de>
2024-12-20 15:40:07 +0100

    strange brhavior

--- lib/email.ts
+++ lib/email.ts
@@ -3,12 +3,12 @@ import { formSchema } from './schema';
 import { Resend } from 'resend';
 import { EmailTemplate } from '@/components/ui/email-template';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_Zpmrt87o_BLQB9FD8ca224x7nD1gUn1HV');
 
 export const send = async (emailFormData: z.infer<typeof formSchema>) => {
 
   const { error } = await resend.emails.send({
-      from: `Acme <${process.env.RESEND_FROM_EMAIL}>`,
+      from: 'Acme <${process.env.RESEND_FROM_EMAIL}>',
       to: [emailFormData.email],
       subject: 'Hello world',
       react: EmailTemplate({ firstName: emailFormData.firstname }),
