3012fd7af55f16ffb21ddeb0581594e80289e6d8
Bernd Ullmann <info@berndullmann.de>
2024-12-30 14:31:47 +0100

    email template

--- lib/email.ts
+++ lib/email.ts
@@ -1,17 +1,16 @@
+'use server';
+
 import { z } from 'zod'
 import { formSchema } from './schema';
 import { Resend } from 'resend';
 import { EmailTemplate } from '@/components/ui/email-template';
-
-console.log('RESEND_API_KEY:', process.env.RESEND_API_KEY);
-console.log('RESEND_FROM_EMAIL:', process.env.RESEND_FROM_EMAIL);
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const send = async (emailFormData: z.infer<typeof formSchema>) => {
 
   const { data, error } = await resend.emails.send({
-      from: `Acme <${process.env.RESEND_FROM_EMAIL}>`,
+      from: `Acme <${process.env.NEXT_PUBLIC_RESEND_FROM_EMAIL}>`,
       to: [emailFormData.email],
       subject: 'Hello world',
       react: EmailTemplate({ firstName: emailFormData.firstname }),
