57d571852c10c2d53ab2e3322666b156dd86af26
Bernhard Ullmann <ullmann@neubaukompass.de>
2026-01-21 14:20:31 +0100

    databse connection

--- netlify/functions/getDocs.ts
+++ netlify/functions/getDocs.ts
@@ -1,10 +1,15 @@
-import { prisma } from '../../lib/prisma'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
 
 export const handler = async () => {
   try {
+    const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
+    const prisma = new PrismaClient({ adapter })
+
     const docs = await prisma.doc.findMany()
     return {
       statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(docs),
     }
   } catch (err) {
